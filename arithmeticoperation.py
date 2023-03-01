fa9718da7d6370115517699aabc2023bf8a6f954
Abhishektk2002 <126573503+Abhishektk2002@users.noreply.github.com>
2023-03-01 17:33:17 +0530

    Add files via upload

---
+++ arithmeticoperation.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the value of x \t"))
+y=int(input("Enter the value of y \t"))
+z=input("Enter the operation to perform \n")
+if z=="+":
+	print(x,"+",y,"=",x+y)
+elif z=="-":
+	print(x,"-",y,"=",x-y)
+else :
+	print("invalid input")
