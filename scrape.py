e3c0e39cafb817f2ab65f2417a8df5bc23576ceb
Abhishektk2002 <126573503+Abhishektk2002@users.noreply.github.com>
2023-04-04 18:13:57 +0530

    Add files via upload

--- scrape.py
+++ scrape.py
@@ -1,17 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-req=requests.get("https://www.cet.ac.in/head-of-departments/")
-
-source_code=req.content
-soup=BeautifulSoup(source_code,"lxml")
-figure_element=soup.find("figure",class_="wp-block-table")
-table=figure_element.find("table")
-
-table_rows=table.find_all("tr")
-table_rows=table_rows[1:]
-
+req = requests.get("https://www.cet.ac.in/head-of-departments/")
+source = req.content
+soup = BeautifulSoup(source,"lxml")
+figure = soup.find("figure",class_="wp-block-table")
+table = figure.find("table")
+table_rows = table.find_all("tr")
+table_rows = table_rows[1:]
 for row in table_rows:
-        tds = row.find_all("td")
-        name = tds[0].text
-        print(name)
+    tds = row.find_all("td")
+    n = tds[0].text
+    m = tds[1].text
+    s = tds[2].text
+    print(n,m,s)
